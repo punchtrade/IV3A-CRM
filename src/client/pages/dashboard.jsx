f6a5c2f169fb27f642e3699ef41dcfcac3a3b553
Carmen <carmenbuendiamusic@gmail.com>
2020-11-30 13:14:47 +0100

    commit seguimos con los arreglos

--- src/client/pages/dashboard.jsx
+++ src/client/pages/dashboard.jsx
@@ -4,7 +4,7 @@ import Scheduler from '../components/scheduler/scheduler';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { loginUser } from "/Users/carmenbuendia/DevProjects/IV3A/src/client/actions/authActions.js";
+import { loginUser } from "../actions/authActions";
 
 const useStyles = makeStyles(theme => ({
   dashboardContainer: {
